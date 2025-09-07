64f38fa9f7c5c71103d9db321a169b294cf1ff5a
Eliab <35658748+eliabsiji@users.noreply.github.com>
2025-09-07 18:20:50 +0100

    Update team.blade.php

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -20,113 +20,164 @@
         <!--Team Page Start-->
         <section class="team-page">
            
-  <!--Team One Start-->
-  <section class="team-one">
-    <div class="container">
-        <div class="section-title text-center">
-            <span class="section-title__tagline">Our Best Expert</span>
-            <div class="section-title-shape">
-                <img src="{{ asset('assets/assets/images/shapes/section-title-shape-1.png')}}" alt="">
-            </div>
-            <h2 class="section-title__title">Meet the team members</h2>
-        </div>
-        <div class="row">
+        <!--Team One Start-->
+        <section class="team-one">
+            <div class="container">
+                <div class="section-title text-center">
+                    <span class="section-title__tagline">Our Best Expert</span>
+                    <div class="section-title-shape">
+                        <img src="{{ asset('assets/assets/images/shapes/section-title-shape-1.png')}}" alt="">
+                    </div>
+                    <h2 class="section-title__title">Meet the team members</h2>
+                </div>
+                <div class="row">
 
-              
-   <!--Team One Single Start-->
-            <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="200ms">
-                <div class="team-one__single">
-                    <div class="team-one__inner">
-                        <div class="team-one__shape-1 float-bob-y"></div>
-                        <div class="team-one__img">
-                            <img src="{{ asset('assets/assets/images/team/4.jpg')}}" alt="">
-                        </div>
-                        <div class="team-one__content">
-                            <div class="team-one__rating">
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
+                    
+        <!--Team One Single Start-->
+                    <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="200ms">
+                        <div class="team-one__single">
+                            <div class="team-one__inner">
+                                <div class="team-one__shape-1 float-bob-y"></div>
+                                <div class="team-one__img">
+                                    <img src="{{ asset('assets/assets/images/team/4.jpg')}}" alt="">
+                                </div>
+                                <div class="team-one__content">
+                                    <div class="team-one__rating">
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                    </div>
+                                    <h3 class="team-one__name"><a href="#">Mr Philip Oklu</a></h3>
+                                    <p class="team-one__sub-title">Principal</p>
+                                    {{-- <div class="team-one__social">
+                                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                        <a href="#"><i class="fab fa-instagram"></i></a>
+                                        <a href="#"><i class="fab fa-twitter"></i></a>
+                                    </div> --}}
+                                </div>
                             </div>
-                            <h3 class="team-one__name"><a href="#">Mr Philip Oklu</a></h3>
-                            <p class="team-one__sub-title">Principal</p>
-                            {{-- <div class="team-one__social">
-                                <a href="#"><i class="fab fa-facebook-f"></i></a>
-                                <a href="#"><i class="fab fa-instagram"></i></a>
-                                <a href="#"><i class="fab fa-twitter"></i></a>
-                            </div> --}}
                         </div>
                     </div>
-                </div>
-            </div>
-            <!--Team One Single End-->
-  
-            <!--Team One Single Start-->
-            <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="100ms">
-                <div class="team-one__single">
-                    <div class="team-one__inner">
-                        <div class="team-one__shape-1 float-bob-y"></div>
-                        <div class="team-one__img">
-                            <img src="{{ asset('assets/assets/images/team/1.jpg')}}" alt="">
-                        </div>
-                        <div class="team-one__content">
-                            <div class="team-one__rating">
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
+                    <!--Team One Single End-->
+        
+                    <!--Team One Single Start-->
+                    <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="100ms">
+                        <div class="team-one__single">
+                            <div class="team-one__inner">
+                                <div class="team-one__shape-1 float-bob-y"></div>
+                                <div class="team-one__img">
+                                    <img src="{{ asset('assets/assets/images/team/1.jpg')}}" alt="">
+                                </div>
+                                <div class="team-one__content">
+                                    <div class="team-one__rating">
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                    </div>
+                                    <h3 class="team-one__name"><a href="#">Mrs Kofoworola Njoku </a></h3>
+                                    <p class="team-one__sub-title">Vice Principal</p>
+                                    {{-- <div class="team-one__social">
+                                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                        <a href="#"><i class="fab fa-instagram"></i></a>
+                                        <a href="#"><i class="fab fa-twitter"></i></a>
+                                    </div> --}}
+                                </div>
                             </div>
-                            <h3 class="team-one__name"><a href="#">Mrs Kofoworola Njoku </a></h3>
-                            <p class="team-one__sub-title">Vice Principal</p>
-                            {{-- <div class="team-one__social">
-                                <a href="#"><i class="fab fa-facebook-f"></i></a>
-                                <a href="#"><i class="fab fa-instagram"></i></a>
-                                <a href="#"><i class="fab fa-twitter"></i></a>
-                            </div> --}}
                         </div>
                     </div>
-                </div>
-            </div>
-            <!--Team One Single End-->
-           
-            <!--Team One Single Start-->
-            <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="300ms">
-                <div class="team-one__single">
-                    <div class="team-one__inner">
-                        <div class="team-one__shape-1 float-bob-y"></div>
-                        <div class="team-one__img">
-                            <img src="{{ asset('assets/assets/images/team/3.jpg')}}" alt="">
-                        </div>
-                        <div class="team-one__content">
-                            <div class="team-one__rating">
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
+                    <!--Team One Single End-->
+                
+                    <!--Team One Single Start-->
+                    <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="300ms">
+                        <div class="team-one__single">
+                            <div class="team-one__inner">
+                                <div class="team-one__shape-1 float-bob-y"></div>
+                                <div class="team-one__img">
+                                    <img src="{{ asset('assets/assets/images/team/3.jpg')}}" alt="">
+                                </div>
+                                <div class="team-one__content">
+                                    <div class="team-one__rating">
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                    </div>
+                                    <h3 class="team-one__name"><a href="#">Lawal Harafat</a></h3>
+                                    <p class="team-one__sub-title">Student President</p>
+                                    {{-- <div class="team-one__social">
+                                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                        <a href="#"><i class="fab fa-instagram"></i></a>
+                                        <a href="#"><i class="fab fa-twitter"></i></a>
+                                    </div> --}}
+                                </div>
                             </div>
-                            <h3 class="team-one__name"><a href="#">Lawal Harafat</a></h3>
-                            <p class="team-one__sub-title">Student President</p>
-                            {{-- <div class="team-one__social">
-                                <a href="#"><i class="fab fa-facebook-f"></i></a>
-                                <a href="#"><i class="fab fa-instagram"></i></a>
-                                <a href="#"><i class="fab fa-twitter"></i></a>
-                            </div> --}}
                         </div>
                     </div>
+                    <!--Team One Single End-->
                 </div>
             </div>
-            <!--Team One Single End-->
-        </div>
-    </div>
-</section>
-<!--Team One End-->
+        </section>
+        <!--Team One End-->
         </section>
         <!--Team Page End-->
 
-   
+        <!--Counter One Start-->
+        <section class="counter-one counter-three">
+             <!--Counter Two Start-->
+            <section class="counter-two">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-12">
+                            <ul class="counter-one__points list-unstyled">
+                                <li>
+                                    <div class="counter-one__points-box">
+                                        <div class="counter-one__icon">
+                                            <span class="icon-newspaper-news"></span>
+                                        </div>
+                                        <div class="counter-one__count-box">
+                                            <h3 class="odometer" data-count="1300">00</h3>
+                                            <span class="counter-one__letter">+</span>
+                                        </div>
+                                    </div>
+                                    <p class="counter-one__text">Prevarsity Alumni</p>
+                                </li>
+                                <li>
+                                    <div class="counter-one__points-box">
+                                        <div class="counter-one__icon">
+                                            <span class="icon-flower-smile"></span>
+                                        </div>
+                                        <div class="counter-one__count-box">
+                                            <h3 class="odometer" data-count="22">00</h3>
+                                            <span class="counter-one__letter">+</span>
+                                        </div>
+                                    </div>
+                                    <p class="counter-one__text">College Students</p>
+                                </li>
+                                <li>
+                                    <div class="counter-one__points-box">
+                                        <div class="counter-one__icon">
+                                            <span class="icon-rating"></span>
+                                        </div>
+                                        <div class="counter-one__count-box">
+                                            <h3 class="odometer" data-count="15">00</h3>
+                                            <span class="counter-one__letter">+</span>
+                                        </div>
+                                    </div>
+                                    <p class="counter-one__text">Teachers</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!--Counter Two End-->
+        </section>
+        <!--Counter One End-->
 
  
 @endsection
