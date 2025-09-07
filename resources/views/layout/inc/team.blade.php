4fab9f9a027efc9246f08b0bdf6405ed69a945f1
Eliab <35658748+eliabsiji@users.noreply.github.com>
2025-09-07 17:20:39 +0100

    Update team.blade.php

--- resources/views/layout/inc/team.blade.php
+++ resources/views/layout/inc/team.blade.php
@@ -1,4 +1,17 @@
-  
+
+  <!--Team One Start-->
+  <section class="team-one">
+    <div class="container">
+        <div class="section-title text-center">
+            <span class="section-title__tagline">Our Best Expert</span>
+            <div class="section-title-shape">
+                <img src="{{ asset('assets/assets/images/shapes/section-title-shape-1.png')}}" alt="">
+            </div>
+            <h2 class="section-title__title">Meet the team members</h2>
+        </div>
+        <div class="row">
+
+              
    <!--Team One Single Start-->
             <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="200ms">
                 <div class="team-one__single">
@@ -28,17 +41,6 @@
             </div>
             <!--Team One Single End-->
   
-  <!--Team One Start-->
-  <section class="team-one">
-    <div class="container">
-        <div class="section-title text-center">
-            <span class="section-title__tagline">Our Best Expert</span>
-            <div class="section-title-shape">
-                <img src="{{ asset('assets/assets/images/shapes/section-title-shape-1.png')}}" alt="">
-            </div>
-            <h2 class="section-title__title">Meet the team members</h2>
-        </div>
-        <div class="row">
             <!--Team One Single Start-->
             <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="100ms">
                 <div class="team-one__single">
