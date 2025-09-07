db4ac12886032bf743333e02b55531bf163fb068
Eliab <35658748+eliabsiji@users.noreply.github.com>
2025-09-07 17:57:25 +0100

    Update services.blade.php

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -21,158 +21,81 @@
         </section>
         <!--Page Header End-->
 
-        <!--Services Page Start-->
-        <section class="services-page">
-            <div class="container">
-                <div class="row">
-                    <!--Services Two Single Start-->
-                    <div class="col-xl-4 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="100ms">
-                        <div class="services-two__single">
-                            <div class="services-two__img">
-                                <img src="assets/images/services/services-2-1.jpg" alt="">
-                                <div class="services-two__icon">
-                                    <span class="icon-diagram-business"></span>
+          <!--Services Three start-->
+            <section class="services-three">
+                <div class="container">
+                    <div class="section-title text-center">
+                        <span class="section-title__tagline">Our Schools</span>
+                        <div class="section-title-shape">
+                            <img src="{{ asset('assets/assets/images/shapes/section-title-shape-1.png')}}" alt="">
+                        </div>
+                        <h2 class="section-title__title">Academia O Group of Schools</h2>
+                    </div>
+                    <div class="row">
+                        <!--services One single start-->
+                        <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="100ms">
+                            <div class="services-one__single">
+                                <div class="services-one__count"></div>
+                                <div class="services-one__content">
+                                    <div class="services-one__title">
+                                        <h3><a href="#">Junior High  School</a></h3>
+                                    </div>
+                                    {{-- <div class="services-one__icon">
+                                        <span class="icon-bulb-business-idea"></span>
+                                    </div> --}}
                                 </div>
-                            </div>
-                            <div class="services-two__content-box">
-                                <div class="services-two__count"></div>
-                                <div class="services-two__title">
-                                    <h3><a href="branding.html">Creative design <br> Brand strategy</a></h3>
-                                </div>
-                                <p class="services-two__text">Lorem Ipsum generators on the Internet tend to repeat
-                                    agency solution</p>
-                                <div class="services-two__btn-box">
-                                    <a href="branding.html" class="thm-btn">Read more<i
-                                            class="icon-right-arrow1"></i></a>
+                                <p class="services-one__text">Redefining the goal of education from the conventional, nurturing students of Year 7 to Year 9.</p>
+                                <div class="services-one__btn">
+                                    {{-- <a href="branding.html')}}" class="thm-btn services-three__btn">Read more<i
+                                            class="icon-right-arrow1"></i></a> --}}
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <!--Services Two Single End-->
-                    <!--Services Two Single Start-->
-                    <div class="col-xl-4 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="200ms">
-                        <div class="services-two__single">
-                            <div class="services-two__img">
-                                <img src="assets/images/services/services-2-2.jpg" alt="">
-                                <div class="services-two__icon">
-                                    <span class="icon-layers"></span>
+                        <!--services One single End-->
+                        <!--services One single start-->
+                        <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="200ms">
+                            <div class="services-one__single">
+                                <div class="services-one__count"></div>
+                                <div class="services-one__content">
+                                    <div class="services-one__title">
+                                        <h3><a href="#">Senior High  School</a>
+                                        </h3>
+                                    </div>
+                                    {{-- <div class="services-one__icon">
+                                        <span class="icon-business-management-target"></span>
+                                    </div> --}}
                                 </div>
-                            </div>
-                            <div class="services-two__content-box">
-                                <div class="services-two__count"></div>
-                                <div class="services-two__title">
-                                    <h3><a href="product-development.html">Content Production
-                                            <br> Brand Design</a></h3>
-                                </div>
-                                <p class="services-two__text">Lorem Ipsum generators on the Internet tend to repeat
-                                    agency solution</p>
-                                <div class="services-two__btn-box">
-                                    <a href="product-development.html" class="thm-btn">Read more<i
-                                            class="icon-right-arrow1"></i></a>
+                                <p class="services-one__text">Using technology and modern tools with consistent science practicals to prepare Year 10 to Year 12 students for the task ahead.</p>
+                                <div class="services-one__btn">
+                                    {{-- <a href="#" class="thm-btn services-three__btn">Read more<i
+                                            class="icon-right-arrow1"></i></a> --}}
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <!--Services Two Single End-->
-                    <!--Services Two Single Start-->
-                    <div class="col-xl-4 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="300ms">
-                        <div class="services-two__single">
-                            <div class="services-two__img">
-                                <img src="assets/images/services/services-2-3.jpg" alt="">
-                                <div class="services-two__icon">
-                                    <span class="icon-megaphone"></span>
+                        <!--services One single End-->
+                        <!--services One single start-->
+                        <div class="col-xl-4 col-lg-4 wow fadeInUp" data-wow-delay="300ms">
+                            <div class="services-one__single">
+                                <div class="services-one__count"></div>
+                                <div class="services-one__content">
+                                    <div class="services-one__title">
+                                        <h3><a href="#">Prevarsity</a></h3>
+                                    </div>
+                                    {{-- <div class="services-one__icon">
+                                        <span class="icon-megaphone"></span>
+                                    </div> --}}
                                 </div>
-                            </div>
-                            <div class="services-two__content-box">
-                                <div class="services-two__count"></div>
-                                <div class="services-two__title">
-                                    <h3><a href="digital-marketing.html">Commercials
-                                            <br> Digital marketing</a></h3>
-                                </div>
-                                <p class="services-two__text">Lorem Ipsum generators on the Internet tend to repeat
-                                    agency solution</p>
-                                <div class="services-two__btn-box">
-                                    <a href="digital-marketing.html" class="thm-btn">Read more<i
-                                            class="icon-right-arrow1"></i></a>
+                                <p class="services-one__text">Preparing high school leavers for admission based examinations towards a brighter future in higher institution.</p>
+                                <div class="services-one__btn">
+                                    {{-- <a href="digital-solutions.html')}}" class="thm-btn services-three__btn">Read more<i
+                                            class="icon-right-arrow1"></i></a> --}}
                                 </div>
                             </div>
                         </div>
+                        <!--services One single End-->
                     </div>
-                    <!--Services Two Single End-->
-                    <!--Services Two Single Start-->
-                    <div class="col-xl-4 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="400ms">
-                        <div class="services-two__single">
-                            <div class="services-two__img">
-                                <img src="assets/images/services/services-2-4.jpg" alt="">
-                                <div class="services-two__icon">
-                                    <span class="icon-megaphone"></span>
-                                </div>
-                            </div>
-                            <div class="services-two__content-box">
-                                <div class="services-two__count"></div>
-                                <div class="services-two__title">
-                                    <h3><a href="digital-marketing.html">Digital Marketing <br> MR/VR</a></h3>
-                                </div>
-                                <p class="services-two__text">Lorem Ipsum generators on the Internet tend to repeat
-                                    agency solution</p>
-                                <div class="services-two__btn-box">
-                                    <a href="digital-marketing.html" class="thm-btn">Read more<i
-                                            class="icon-right-arrow1"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!--Services Two Single End-->
-                    <!--Services Two Single Start-->
-                    <div class="col-xl-4 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="500ms">
-                        <div class="services-two__single">
-                            <div class="services-two__img">
-                                <img src="assets/images/services/services-2-5.jpg" alt="">
-                                <div class="services-two__icon">
-                                    <span class="icon-megaphone"></span>
-                                </div>
-                            </div>
-                            <div class="services-two__content-box">
-                                <div class="services-two__count"></div>
-                                <div class="services-two__title">
-                                    <h3><a href="digital-solutions.html">Design, Global <br> Website System</a></h3>
-                                </div>
-                                <p class="services-two__text">Lorem Ipsum generators on the Internet tend to repeat
-                                    agency solution</p>
-                                <div class="services-two__btn-box">
-                                    <a href="digital-solutions.html" class="thm-btn">Read more<i
-                                            class="icon-right-arrow1"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!--Services Two Single End-->
-                    <!--Services Two Single Start-->
-                    <div class="col-xl-4 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="600ms">
-                        <div class="services-two__single">
-                            <div class="services-two__img">
-                                <img src="assets/images/services/services-2-6.jpg" alt="">
-                                <div class="services-two__icon">
-                                    <span class="icon-megaphone"></span>
-                                </div>
-                            </div>
-                            <div class="services-two__content-box">
-                                <div class="services-two__count"></div>
-                                <div class="services-two__title">
-                                    <h3><a href="product-development.html">Web & Product <br> Development</a></h3>
-                                </div>
-                                <p class="services-two__text">Lorem Ipsum generators on the Internet tend to repeat
-                                    agency solution</p>
-                                <div class="services-two__btn-box">
-                                    <a href="product-development.html" class="thm-btn">Read more<i
-                                            class="icon-right-arrow1"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!--Services Two Single End-->
                 </div>
-            </div>
-        </section>
-        <!--Services Page End-->
+            </section>
+            <!--Services One End-->
+
 @endsection
