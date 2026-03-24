2d6b9216a7424a5f162a6de669033714b4b7aaed
Eliab <35658748+eliabsiji@users.noreply.github.com>
2026-03-24 16:44:11 +0100

    Update header.blade.php

--- resources/views/layout/inc/header.blade.php
+++ resources/views/layout/inc/header.blade.php
@@ -252,7 +252,7 @@
                                 </div>
                             </div>
                         </div>
-                        
+
                     </div>
                 </div>
             </nav>
@@ -261,3 +261,210 @@
         <div class="stricky-header stricked-menu main-menu">
             <div class="sticky-header__content"></div><!-- /.sticky-header__content -->
         </div><!-- /.stricky-header -->
+
+
+         <!--Main Slider Start-->
+        <section class="main-slider clearfix">
+            <div class="swiper-container thm-swiper__slider" data-swiper-options='{"slidesPerView": 1, "loop": true,
+                "effect": "fade",
+                "pagination": {
+                "el": "#main-slider-pagination",
+                "type": "bullets",
+                "clickable": true
+                },
+                "navigation": {
+                "nextEl": "#main-slider__swiper-button-next",
+                "prevEl": "#main-slider__swiper-button-prev"
+                },
+                "autoplay": {
+                "delay": 5000
+                }}'>
+                <div class="swiper-wrapper">
+
+                    <div class="swiper-slide">
+                        <div class="image-layer"
+                            style="background-image: url(assets/images/backgrounds/main-slider-1-1.jpg);"></div>
+                        <!-- /.image-layer -->
+
+                        <div class="main-slider__shape-1 float-bob-x">
+                            <img src="assets/images/shapes/main-slider-shape-1.png" alt="">
+                        </div>
+                        <div class="main-slider__shape-2 zoominout">
+                            <img src="assets/images/shapes/main-slider-shape-2.png" alt="">
+                        </div>
+                        <div class="main-slider__shape-3 float-bob-x">
+                            <img src="assets/images/shapes/main-slider-shape-3.png" alt="">
+                        </div>
+                        <div class="main-slider__shape-4 float-bob-y">
+                            <img src="assets/images/shapes/main-slider-shape-4.png" alt="">
+                        </div>
+                        <div class="main-slider__shape-5 zoominout">
+                            <img src="assets/images/shapes/main-slider-shape-5.png" alt="">
+                        </div>
+                        <div class="main-slider__shape-6 float-bob-y">
+                            <img src="assets/images/shapes/main-slider-shape-6.png" alt="">
+                        </div>
+                        <div class="main-slider__shape-7"></div>
+                        <div class="main-slider__shape-8"></div>
+                        <div class="main-slider__shape-9 float-bob-x"></div>
+
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-xl-12">
+                                    <div class="main-slider__content">
+                                        <h2 class="main-slider__title">Hi there, <br> we're & why</h2>
+                                        <p class="main-slider__text">We are a digital agency that helps brands become
+                                            the digital benchmark.</p>
+                                        <div class="main-slider__btn-box">
+                                            <a href="about.html" class="thm-btn main-slider__btn">Creative Solution</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="image-layer"
+                            style="background-image: url(assets/images/backgrounds/main-slider-1-2.jpg);"></div>
+                        <!-- /.image-layer -->
+
+                        <div class="main-slider__shape-1 float-bob-x">
+                            <img src="assets/images/shapes/main-slider-shape-1.png" alt="">
+                        </div>
+                        <div class="main-slider__shape-2 zoominout">
+                            <img src="assets/images/shapes/main-slider-shape-2.png" alt="">
+                        </div>
+                        <div class="main-slider__shape-3 float-bob-x">
+                            <img src="assets/images/shapes/main-slider-shape-3.png" alt="">
+                        </div>
+                        <div class="main-slider__shape-4 float-bob-y">
+                            <img src="assets/images/shapes/main-slider-shape-4.png" alt="">
+                        </div>
+                        <div class="main-slider__shape-5 zoominout">
+                            <img src="assets/images/shapes/main-slider-shape-5.png" alt="">
+                        </div>
+                        <div class="main-slider__shape-6 float-bob-y">
+                            <img src="assets/images/shapes/main-slider-shape-6.png" alt="">
+                        </div>
+                        <div class="main-slider__shape-7"></div>
+                        <div class="main-slider__shape-8"></div>
+                        <div class="main-slider__shape-9 float-bob-x"></div>
+
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-xl-12">
+                                    <div class="main-slider__content">
+                                        <h2 class="main-slider__title">Hi there, <br> we're & why</h2>
+                                        <p class="main-slider__text">We are a digital agency that helps brands become
+                                            the digital benchmark.</p>
+                                        <div class="main-slider__btn-box">
+                                            <a href="about.html" class="thm-btn main-slider__btn">Creative Solution</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="image-layer"
+                            style="background-image: url(assets/images/backgrounds/main-slider-1-3.jpg);"></div>
+                        <!-- /.image-layer -->
+
+                        <div class="main-slider__shape-1 float-bob-x">
+                            <img src="assets/images/shapes/main-slider-shape-1.png" alt="">
+                        </div>
+                        <div class="main-slider__shape-2 zoominout">
+                            <img src="assets/images/shapes/main-slider-shape-2.png" alt="">
+                        </div>
+                        <div class="main-slider__shape-3 float-bob-x">
+                            <img src="assets/images/shapes/main-slider-shape-3.png" alt="">
+                        </div>
+                        <div class="main-slider__shape-4 float-bob-y">
+                            <img src="assets/images/shapes/main-slider-shape-4.png" alt="">
+                        </div>
+                        <div class="main-slider__shape-5 zoominout">
+                            <img src="assets/images/shapes/main-slider-shape-5.png" alt="">
+                        </div>
+                        <div class="main-slider__shape-6 float-bob-y">
+                            <img src="assets/images/shapes/main-slider-shape-6.png" alt="">
+                        </div>
+                        <div class="main-slider__shape-7"></div>
+                        <div class="main-slider__shape-8"></div>
+                        <div class="main-slider__shape-9 float-bob-x"></div>
+
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-xl-12">
+                                    <div class="main-slider__content">
+                                        <h2 class="main-slider__title">Hi there, <br> we're & why</h2>
+                                        <p class="main-slider__text">We are a digital agency that helps brands become
+                                            the digital benchmark.</p>
+                                        <div class="main-slider__btn-box">
+                                            <a href="about.html" class="thm-btn main-slider__btn">Creative Solution</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="image-layer"
+                            style="background-image: url(assets/images/backgrounds/main-slider-1-4.jpg);"></div>
+                        <!-- /.image-layer -->
+
+                        <div class="main-slider__shape-1 float-bob-x">
+                            <img src="assets/images/shapes/main-slider-shape-1.png" alt="">
+                        </div>
+                        <div class="main-slider__shape-2 zoominout">
+                            <img src="assets/images/shapes/main-slider-shape-2.png" alt="">
+                        </div>
+                        <div class="main-slider__shape-3 float-bob-x">
+                            <img src="assets/images/shapes/main-slider-shape-3.png" alt="">
+                        </div>
+                        <div class="main-slider__shape-4 float-bob-y">
+                            <img src="assets/images/shapes/main-slider-shape-4.png" alt="">
+                        </div>
+                        <div class="main-slider__shape-5 zoominout">
+                            <img src="assets/images/shapes/main-slider-shape-5.png" alt="">
+                        </div>
+                        <div class="main-slider__shape-6 float-bob-y">
+                            <img src="assets/images/shapes/main-slider-shape-6.png" alt="">
+                        </div>
+                        <div class="main-slider__shape-7"></div>
+                        <div class="main-slider__shape-8"></div>
+                        <div class="main-slider__shape-9 float-bob-x"></div>
+
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-xl-12">
+                                    <div class="main-slider__content">
+                                        <h2 class="main-slider__title">Hi there, <br> we're & why</h2>
+                                        <p class="main-slider__text">We are a digital agency that helps brands become
+                                            the digital benchmark.</p>
+                                        <div class="main-slider__btn-box">
+                                            <a href="about.html" class="thm-btn main-slider__btn">Creative Solution</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <!-- If we need navigation buttons -->
+                <div class="main-slider__nav">
+                    <div class="swiper-button-prev" id="main-slider__swiper-button-next">
+                        <i class="icon-right-arrow-1"></i>
+                    </div>
+                    <div class="swiper-button-next" id="main-slider__swiper-button-prev">
+                        <i class="icon-left-arrow-1"></i>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+        <!--Main Slider End-->
