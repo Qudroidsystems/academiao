a8f91143a35eff625e80ce22d8838ae9bd7b8600
Eliab <35658748+eliabsiji@users.noreply.github.com>
2026-03-24 18:10:04 +0100

    Update footer.blade.php

--- resources/views/layout/inc/footer.blade.php
+++ resources/views/layout/inc/footer.blade.php
@@ -115,61 +115,64 @@
 </div><!-- /.page-wrapper -->
 
 
-       <div class="mobile-nav__wrapper">
-        <div class="mobile-nav__overlay mobile-nav__toggler"></div>
-        <!-- /.mobile-nav__overlay -->
-        <div class="mobile-nav__content">
-            <span class="mobile-nav__close mobile-nav__toggler"><i class="fa fa-times"></i></span>
+<div class="mobile-nav__wrapper">
+<div class="mobile-nav__overlay mobile-nav__toggler"></div>
+<!-- /.mobile-nav__overlay -->
+<div class="mobile-nav__content">
+    <span class="mobile-nav__close mobile-nav__toggler"><i class="fa fa-times"></i></span>
 
-            <div class="logo-box">
-                <a href="index.html" aria-label="logo image"><img src="assets/images/resources/footer-logo.png"
-                        width="143" alt="" /></a>
-            </div>
-            <!-- /.logo-box -->
-            <div class="mobile-nav__container"></div>
-            <!-- /.mobile-nav__container -->
+    {{-- <div class="logo-box">
+        <a href="index.html')}}" aria-label="logo image"><img src="{{ asset('assets/assets/images/resources/footer-logo.png')}}"
+                width="143" alt="" /></a>
+    </div> --}}
+    <!-- /.logo-box -->
+    <div class="mobile-nav__container"></div>
+    <!-- /.mobile-nav__container -->
 
-            <ul class="mobile-nav__contact list-unstyled">
-                <li>
-                    <i class="fa fa-envelope"></i>
-                    <a href="mailto:needhelp@packageName__.com">needhelp@bcorz.com</a>
-                </li>
-                <li>
-                    <i class="fa fa-phone-alt"></i>
-                    <a href="tel:666-888-0000">666 888 0000</a>
-                </li>
-            </ul><!-- /.mobile-nav__contact -->
-            <div class="mobile-nav__top">
-                <div class="mobile-nav__social">
-                    <a href="#" class="fab fa-twitter"></a>
-                    <a href="#" class="fab fa-facebook-square"></a>
-                    <a href="#" class="fab fa-pinterest-p"></a>
-                    <a href="#" class="fab fa-instagram"></a>
-                </div><!-- /.mobile-nav__social -->
-            </div><!-- /.mobile-nav__top -->
+    <ul class="mobile-nav__contact list-unstyled">
+        <li>
+            <i class="fa fa-envelope"></i>
+              <a href="mailto:admission@academiaocollege.ng">admission@academiaocollege.ng</a>
+        </li>
+         <li>
+            <i class="fa fa-envelope"></i>
+              <a href="mailto:info@academiaocollege.ng">info@academiaocollege.ng</a>
+        </li>
+        <li>
+            <i class="fa fa-phone-alt"></i>
+            <a href="tel:+2347087767178">+234 (0) 7087767178</a>
+        </li>
+    </ul><!-- /.mobile-nav__contact -->
+    <div class="mobile-nav__top">
+        <div class="mobile-nav__social">
+            <a href="https://www.facebook.com/academiaOfb" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-facebook-square"></a>
+            <a href="#" class="fab fa-instagram"></a>
+        </div><!-- /.mobile-nav__social -->
+    </div><!-- /.mobile-nav__top -->
 
 
 
-        </div>
-        <!-- /.mobile-nav__content -->
-    </div>
-    <!-- /.mobile-nav__wrapper -->
+</div>
+<!-- /.mobile-nav__content -->
+</div>
+<!-- /.mobile-nav__wrapper -->
 
-    <div class="search-popup">
-        <div class="search-popup__overlay search-toggler"></div>
-        <!-- /.search-popup__overlay -->
-        <div class="search-popup__content">
-            <form action="#">
-                <label for="search" class="sr-only">search here</label><!-- /.sr-only -->
-                <input type="text" id="search" placeholder="Search Here..." />
-                <button type="submit" aria-label="search submit" class="thm-btn">
-                    <i class="icon-search"></i>
-                </button>
-            </form>
-        </div>
-        <!-- /.search-popup__content -->
-    </div>
-    <!-- /.search-popup -->
+<div class="search-popup">
+<div class="search-popup__overlay search-toggler"></div>
+<!-- /.search-popup__overlay -->
+<div class="search-popup__content">
+    <form action="#">
+        <label for="search" class="sr-only">search here</label><!-- /.sr-only -->
+        <input type="text" id="search" placeholder="Search Here..." />
+        <button type="submit" aria-label="search submit" class="thm-btn">
+            <i class="icon-search"></i>
+        </button>
+    </form>
+</div>
+<!-- /.search-popup__content -->
+</div>
+<!-- /.search-popup -->
 
 <a href="#" data-target="html" class="scroll-to-target scroll-to-top"><i class="fa fa-angle-up"></i></a>
 
@@ -177,7 +180,7 @@
 
 <!-- Scripts -->
     <script src="{{ asset('assets/assets/vendors/jquery/jquery-3.6.0.min.js') }}"></script>
-    {{-- @stack('scripts')  <!-- Custom JS (e.g., gallery modal) loads here, after jQuery --> --}}
+  
     <script src="{{ asset('assets/assets/vendors/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('assets/assets/vendors/jarallax/jarallax.min.js') }}"></script>
     <script src="{{ asset('assets/assets/vendors/jquery-ajaxchimp/jquery.ajaxchimp.min.js') }}"></script>
